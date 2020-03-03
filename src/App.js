4e8658379158825bcdaca0b731d3f7229796c0c5
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-03 18:45:21 -0500

    cleaned up POST mode elements since I am not using them rn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
 
   // initialize antd elements
   const { Title, Text, Paragraph} = Typography;
-  const { TextArea } = Input;
   const {Meta} = Card;
 
   // initialize state variables
@@ -17,10 +16,7 @@ function App() {
     channelURL: '',
     textArea: ''
   })
-  const [mode, setMode] = useState(true)  // where fetchMode = true
   const inputEl = useRef(null); // creating a ref for input elements
-  const textEl = useRef(null);
-
   const [persistedData, setPersistedData] = usePersistedState('fetchedData',[]);
 
   const themes = {
@@ -35,8 +31,6 @@ function App() {
   };
   const { theme, toggleTheme } = useContext(ThemeContext);
   const [fontColor, setFontColor] = useState('');
-
-  const token = '' // are.na token in order to POST, not needed right now
 
   function handleToggle() {  
     // toggles the mode, empties data array, resets channelURL, clears inputEl field
@@ -82,37 +76,6 @@ function App() {
     .catch(error => console.log(error))
   }
 
-  const postData = async () => {
-    setState((prevState) => ({...prevState, loading: true})); 
-
-    fetch(`https://api.are.na/v2/channels/${state.channelURL}/blocks`,{
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      //make sure to serialize your JSON body
-      body: JSON.stringify({
-        content: `${state.textArea}`
-      })
-    })
-    .then( (response) => { 
-      response.json()
-    })
-    .then( (data) => {
-      console.log(data)
-      setState((prevState) => ({
-        ...prevState, 
-        loading: false,
-        channelURL: '',
-        textArea: ''
-      })); 
-      inputEl.current.setValue('')
-      textEl.current.setValue('')
-    })
-    .catch(error => console.log(error))
-  }
-
   useEffect(()=> {
     document.body.style.color = themes[theme].font;
     document.body.style.backgroundColor = themes[theme].background;
@@ -128,87 +91,49 @@ function App() {
             <Paragraph style={{ color: `${fontColor}` }} type="secondary">Enter an <a href='https://www.are.na/' target='_blank' rel="noopener noreferrer">are.na</a> channel of your choice to fetch its images and display them below. You can copy and paste the following URL to get started. <Text copyable style={{ color: `${fontColor}` }} code>https://www.are.na/kalli-retzepi/mais-oui-images</Text></Paragraph>
           </Col>
       </Row>  
-      {/*     FETCH MODE      */}
-      {mode && 
-        <div className='fetchMode' >
-          <Row >
-            <Col span={10}>
-              <Input 
-              autoFocus 
-              placeholder="Enter the channel URL" 
-              ref={inputEl}
-              onChange={event => {
-                let inputString = event.target.value.split('/')
-                setState((prevState) => ({...prevState, channelURL: inputString[inputString.length-1]}));
-              }}
-              /> 
-            </Col>
-          </Row>      
-          <Button style={{ margin: '2vh 0'}} onClick={getData} loading={state.loading}> Get channel contents </Button>
         
-          <div className='cards'>
-            {persistedData !== undefined && 
-              persistedData.map( (item, i) => (
-              <Card
-                key={i}
-                style={{ 
-                  width: '20%', 
-                  alignSelf: 'stretch', 
-                  margin: '1vh 1vw 1vh 0',
-                }}
-                cover={
-                  <img
-                    alt="example"
-                    src={item.image.thumb.url}
-                  />
-                }>
-                <Meta
-                  title= {item.title}
-                  description={item.created_at}
-                />
-              </Card>
-              ))
-            }
-          </div>
-        </div>
-      }
-
-      {/*     POST MODE - disabled for now -       */}
-      {!mode && 
-        <div className='putMode'>
-          <Row style={{ margin: '16px 0' }}>
-            <Col span={12}>
-              <Input 
-              autoFocus 
-              ref={inputEl}
-              placeholder="Enter the channel URL" 
-              onChange={event => {
-                let inputString = event.target.value.split('/')
-                setState((prevState) => ({...prevState, channelURL: inputString[inputString.length-1]}));
+      <div className='fetchMode' >
+        <Row >
+          <Col span={10}>
+            <Input 
+            autoFocus 
+            placeholder="Enter the channel URL" 
+            ref={inputEl}
+            onChange={event => {
+              let inputString = event.target.value.split('/')
+              setState((prevState) => ({...prevState, channelURL: inputString[inputString.length-1]}));
+            }}
+            /> 
+          </Col>
+        </Row>      
+        <Button style={{ margin: '2vh 0'}} onClick={getData} loading={state.loading}> Get channel contents </Button>
+      
+        <div className='cards'>
+          {persistedData !== undefined && 
+            persistedData.map( (item, i) => (
+            <Card
+              key={i}
+              style={{ 
+                width: '20%', 
+                alignSelf: 'stretch', 
+                margin: '1vh 1vw 1vh 0',
               }}
-              /> 
-            </Col>
-          </Row>      
+              cover={
+                <img
+                  alt="example"
+                  src={item.image.thumb.url}
+                />
+              }>
+              <Meta
+                title= {item.title}
+                description={item.created_at}
+              />
+            </Card>
+            ))
+          }
+        </div>
+      </div>
 
-          <Row style={{ margin: '16px 0' }}>
-            <Col span={12}>
-              <TextArea 
-                ref={textEl}
-                autoFocus 
-                placeholder="Enter your text" 
-                onChange={event => {
-                  let inputString = event.target.value
-                  setState((prevState) => ({...prevState, textArea: inputString}));
-                }}
-                rows={4} />
-            </Col>
-          </Row>
-
-          <Button style={{ marginBottom: '2vh'}} onClick={postData} loading={state.loading}> Add to channel </Button>
-        </div>      
-       }
-
-  
       {/*     TOGGLE        */}
       <Row style={{ margin: '45vh 0 5vh 0'}}>
         <Col span={6}>
